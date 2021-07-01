1b90f87d23ebccd392a89bb6d28dc0c9d7f8f962
Kijacode <kijadanford@gmail.com>
2021-07-01 16:53:00 +0300

    add log message

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/ussd', (req, res) => {
+app.get('/ussd', (req, res) => {
     // Read the variables sent via POST from our API
     const {
         sessionId,
