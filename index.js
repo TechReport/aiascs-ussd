61b97e19279a31b3eb5a62bc80281d5eb5a3a3f9
Kijacode <kijadanford@gmail.com>
2021-07-02 15:10:01 +0300

    feat: add termination text

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ app.post('/ussd', (req, res) => {
     if (text == '') {
         // This is the first request. Note how we start the response with CON
         response = `CON Karibu Katika mfumo wa AIASCS ,chagua lugha ili kuendelea 
-        1. Kiswahili
-        2. English `;
+        1. Kiswahili`;
     } else if ( text == '1') {
         // Business logic for first level response
         response = `CON Chagua huduma unayohitaji kati ya huduma zetu zifuatazo
@@ -29,8 +28,7 @@ app.post('/ussd', (req, res) => {
         4. Kutoa Mrejesho `;
         if( text == '3')
         {
-            response = `CON Ingiza namba ya kuhakiki pembejeo hapa chini
-            0. Namba Ni  `;
+            response = `CON Ingiza namba ya kuhakiki pembejeo hapa chini `;
             // number hii hapa
         }
         else if(text == '4')
@@ -38,15 +36,10 @@ app.post('/ussd', (req, res) => {
             response = `CON Ingiza mrejesho wako hapa chini `;
           
         }
-    } else if ( text == '2') {
-        // Business logic for first level response
+    }  else{
+
         // This is a terminal request. Note how we start the response with END
-        response = `END Your phone number is ${phoneNumber}`;
-    } else if ( text == '1*1') {
-        // This is a second level response where the user selected 1 in the first instance
-        const accountNumber = 'ACC100101';
-        // This is a terminal request. Note how we start the response with END
-        response = `END Your account number is ${accountNumber}`;
+        response = `END Asante na karibu tena, tunakutakia majukumu mema katika uzalishaji wa chakula`;
     }
 
     // Send the response back to the API
